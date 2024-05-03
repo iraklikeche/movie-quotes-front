5317e3260abb63cb0c27a564164c3e637517d947
iraklikeche <irakli.kechee@gmail.com>
2024-05-04 00:48:49 +0400

    style/mobile profileView

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,11 +1,68 @@
 <template>
   <TheLayout :customHeight="'h-screen'">
-    <div class="sm:hidden py-6 bg-[#11101A]">
+    <!-- I know it's ugly I will refactor it tomorrow -->
+    <div class="pt-12 px-8" v-if="finalCheck">
+      <div class="bg-[#24222f] rounded-xl">
+        <div class="py-12 flex items-center justify-center border-b border-[#CED4DA]">
+          <p class="text-white">Are you sure to make changes?</p>
+        </div>
+        <div class="flex justify-between py-4 px-5">
+          <button class="text-[#ced4da]" @click="closeEditFrom">Cancel</button>
+          <button class="bg-[#E31221] text-white px-2 py-1.5 rounded-md" @click="onSubmit">
+            Confirm
+          </button>
+        </div>
+      </div>
+    </div>
+
+    <div class="sm:hidden py-6 bg-[#11101A]" v-if="!finalCheck">
       <div class="px-8">
         <GoBackBtn />
       </div>
     </div>
-    <div>
+    <div v-if="edit">
+      <div class="bg-[#24222F] py-20 flex flex-col px-8">
+        <label class="text-white mb-1">Enter new username</label>
+        <input type="text" class="bg-[#CED4DA] py-2 rounded-md pl-4" v-model="name" />
+      </div>
+      <div class="px-8 flex justify-between mt-6">
+        <button class="text-[#CED4DA]" @click="closeEditFrom">Cancel</button>
+        <button class="bg-[#E31221] text-white px-5 py-1.5 rounded-md" @click="handleEdit">
+          Edit
+        </button>
+      </div>
+    </div>
+    <div v-if="reset">
+      <div class="bg-[#24222F] py-20 px-8">
+        <Form class="flex flex-col gap-5">
+          <CustomInput
+            :label="$t('sessions.password')"
+            name="password"
+            rules="required|min:3"
+            :placeholder="$t('sessions.password_placeholder')"
+            isPasswordField
+          />
+
+          <CustomInput
+            type="password"
+            :label="$t('sessions.conf_password')"
+            name="password_confirmation"
+            :placeholder="$t('sessions.conf_password_placeholder')"
+            isPasswordField
+            rules="confirmed:@password_confirmation"
+          />
+        </Form>
+      </div>
+
+      <div class="px-8 flex justify-between mt-6">
+        <button class="text-[#CED4DA]" @click="closeEditFrom">Cancel</button>
+        <button class="bg-[#E31221] text-white px-5 py-1.5 rounded-md" @click="handleEdit">
+          Edit
+        </button>
+      </div>
+    </div>
+
+    <div v-if="foo">
       <span class="hidden sm:block text-white text-2xl font-medium mb-28 pl-10">{{
         $t('texts.my_profile')
       }}</span>
@@ -17,7 +74,7 @@
           <p class="text-white text-xl">{{ $t('texts.upload_photo') }}</p>
         </div>
         <div class="px-8 mt-16">
-          <form @submit="onSubmit" class="flex flex-col sm:mt-32 sm:px-48 gap-8">
+          <div class="flex flex-col sm:mt-32 sm:px-48 gap-8">
             <div class="flex flex-col">
               <label class="text-white mb-1">{{ $t('sessions.username') }}</label>
               <div class="relative sm:flex sm:items-center gap-8">
@@ -28,7 +85,8 @@
                   disabled
                 />
                 <span
-                  class="text-[#CED4DA] absolute sm:static right-0 top-[40%] -translate-y-1/2 sm:-translate-y-0"
+                  @click="openEditForm"
+                  class="text-[#CED4DA] absolute sm:static right-0 top-[40%] -translate-y-1/2 sm:-translate-y-0 cursor-pointer"
                   >{{ $t('texts.edit') }}</span
                 >
               </div>
@@ -59,12 +117,13 @@
                   disabled
                 />
                 <span
+                  @click="openResetPasswordForm"
                   class="text-[#CED4DA] absolute sm:static right-0 top-[40%] -translate-y-1/2 sm:-translate-y-0 sm:py-1"
                   >{{ $t('texts.edit') }}</span
                 >
               </div>
             </div>
-          </form>
+          </div>
         </div>
       </div>
     </div>
@@ -75,27 +134,47 @@
 import GoBackBtn from '@/components/icons/GoBackBtn.vue'
 import profile from '@/assets/images/profile.png'
 import TheLayout from '@/components/TheLayout.vue'
-import { ref, onMounted, watch } from 'vue'
-import { useForm, Field } from 'vee-validate'
+import { ref, onMounted } from 'vue'
+import { useForm, Field, Form } from 'vee-validate'
 import { useUserSessionStore } from '@/stores/UserSessionStore'
+import CustomInput from '@/components/Form/CustomInput.vue'
 
 const userSession = useUserSessionStore()
 
 const username = ref('')
 const email = ref('')
+const edit = ref(false)
+const name = ref('')
+const finalCheck = ref(false)
+const foo = ref(true)
+const reset = ref(false)
+const { setValues } = useForm()
 
-const { handleSubmit, values, setValues } = useForm()
+const handleEdit = () => {
+  edit.value = false
+  finalCheck.value = true
+  reset.value = false
+}
 
-watch(
-  () => username.value,
-  (newValue, oldValue) => {
-    console.log(newValue, oldValue)
-  }
-)
+const openResetPasswordForm = () => {
+  reset.value = true
+  foo.value = false
+}
 
-const onSubmit = handleSubmit((values) => {
-  console.log(values)
-})
+const openEditForm = () => {
+  foo.value = false
+  edit.value = true
+}
+
+const closeEditFrom = () => {
+  foo.value = true
+  edit.value = false
+  finalCheck.value = false
+}
+
+const onSubmit = () => {
+  closeEditFrom()
+}
 
 onMounted(async () => {
   if (localStorage.getItem('isLoggedIn')) {
