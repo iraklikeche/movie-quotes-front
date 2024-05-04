4f221b48412c7565c0c5fe01ee8057b2b0b66556
iraklikeche <irakli.kechee@gmail.com>
2024-05-05 00:48:04 +0400

    style:hamburger-menu

--- src/components/TheLayout.vue
+++ src/components/TheLayout.vue
@@ -1,4 +1,30 @@
 <template>
+  <div class="bg-[#11101A] h-[41rem] w-[90%] absolute flex flex-col rounded-lg">
+    <div class="flex flex-col gap-12 p-8">
+      <div class="flex items-center gap-4 text-white">
+        <img src="https://picsum.photos/200" class="w-14 rounded-full border-2" />
+        <div>
+          <p>{{ username }}</p>
+          <RouterLink :to="{ name: 'profile' }" class="text-[#ced4da]">{{
+            $t('texts.edit_profile')
+          }}</RouterLink>
+        </div>
+      </div>
+      <div class="flex gap-8 items-center text-white pl-2">
+        <NewsFeed />
+        <p>{{ $t('texts.news_feed') }}</p>
+      </div>
+      <div class="flex gap-8 items-center text-white pl-2">
+        <ListOfMovies />
+        <p>{{ $t('texts.movie_list') }}</p>
+      </div>
+    </div>
+    <div class="mt-auto p-8">
+      <button @click="onLogout" class="text-white bg-[#e31221] px-3 py-1.5 rounded-md">
+        Log out
+      </button>
+    </div>
+  </div>
   <TheHeader class="bg-[#242229]" />
   <main :class="['bg-[#171623] w-full sm:px-16 sm:py-8 sm:flex', customHeight]">
     <div class="hidden sm:flex flex-col gap-8 min-w-56">
@@ -36,11 +62,24 @@ import { onMounted, ref } from 'vue'
 import NewsFeed from '@/components/icons/NewsFeed.vue'
 import TheHeader from '@/components/TheHeader.vue'
 import { useUserSessionStore } from '@/stores/UserSessionStore'
-import { RouterLink } from 'vue-router'
+import { RouterLink, useRouter } from 'vue-router'
+import { getCsrfCookie, logoutUser } from '@/service/authService'
 
 const props = defineProps(['customHeight'])
 const userSession = useUserSessionStore()
 const username = ref('')
+const router = useRouter()
+
+const onLogout = async () => {
+  await getCsrfCookie()
+  try {
+    await logoutUser()
+    localStorage.removeItem('isLoggedIn')
+    router.push('/')
+  } catch (error) {
+    //
+  }
+}
 
 onMounted(async () => {
   if (localStorage.getItem('isLoggedIn')) {
--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,40 +1,48 @@
 <template>
   <TheLayout :customHeight="'h-screen'">
-    <!-- I know it's ugly I will refactor it tomorrow -->
-    <div class="pt-12 px-8" v-if="finalCheck">
+    <div class="pt-12 px-8" v-if="currentMode === Mode.FINAL_CHECK">
       <div class="bg-[#24222f] rounded-xl">
         <div class="py-12 flex items-center justify-center border-b border-[#CED4DA]">
           <p class="text-white">Are you sure to make changes?</p>
         </div>
         <div class="flex justify-between py-4 px-5">
-          <button class="text-[#ced4da]" @click="closeEditFrom">Cancel</button>
-          <button class="bg-[#E31221] text-white px-2 py-1.5 rounded-md" @click="onSubmit">
+          <button class="text-[#ced4da]" @click="closeEditForm">Cancel</button>
+          <button class="bg-[#E31221] text-white px-2 py-1.5 rounded-md" @click="foo">
             Confirm
           </button>
         </div>
       </div>
     </div>
 
-    <div class="sm:hidden py-6 bg-[#11101A]" v-if="!finalCheck">
+    <div class="sm:hidden py-6 bg-[#11101A]" v-if="currentMode !== Mode.FINAL_CHECK">
       <div class="px-8">
-        <GoBackBtn />
+        <GoBackBtn class="cursor-pointer" @click="goBack" />
       </div>
     </div>
-    <div v-if="edit">
-      <div class="bg-[#24222F] py-20 flex flex-col px-8">
-        <label class="text-white mb-1">Enter new username</label>
-        <input type="text" class="bg-[#CED4DA] py-2 rounded-md pl-4" v-model="name" />
-      </div>
+
+    <!-- Username -->
+    <div v-if="currentMode === Mode.EDIT">
+      <form class="bg-[#24222F] py-20 flex flex-col px-8">
+        <CustomInput
+          :label="$t('sessions.username')"
+          name="username"
+          :placeholder="$t('sessions.username_placeholder')"
+          rules="required|min:3"
+          type="text"
+        />
+      </form>
       <div class="px-8 flex justify-between mt-6">
-        <button class="text-[#CED4DA]" @click="closeEditFrom">Cancel</button>
-        <button class="bg-[#E31221] text-white px-5 py-1.5 rounded-md" @click="handleEdit">
+        <button class="text-[#CED4DA]" @click="closeEditForm">Cancel</button>
+        <button class="bg-[#E31221] text-white px-5 py-1.5 rounded-md" @click="confirmEdit">
           Edit
         </button>
       </div>
     </div>
-    <div v-if="reset">
+
+    <!-- Reset -->
+    <div v-if="currentMode === Mode.RESET">
       <div class="bg-[#24222F] py-20 px-8">
-        <Form class="flex flex-col gap-5">
+        <form @submit="onSubmit" class="flex flex-col gap-5">
           <CustomInput
             :label="$t('sessions.password')"
             name="password"
@@ -51,22 +59,23 @@
             isPasswordField
             rules="confirmed:@password_confirmation"
           />
-        </Form>
+        </form>
       </div>
 
       <div class="px-8 flex justify-between mt-6">
-        <button class="text-[#CED4DA]" @click="closeEditFrom">Cancel</button>
-        <button class="bg-[#E31221] text-white px-5 py-1.5 rounded-md" @click="handleEdit">
+        <button class="text-[#CED4DA]" @click="closeEditForm">Cancel</button>
+        <button class="bg-[#E31221] text-white px-5 py-1.5 rounded-md" @click="confirmReset">
           Edit
         </button>
       </div>
     </div>
 
-    <div v-if="foo">
+    <!-- Main -->
+    <div v-if="currentMode === Mode.MAIN">
       <span class="hidden sm:block text-white text-2xl font-medium mb-28 pl-10">{{
         $t('texts.my_profile')
       }}</span>
-      <div class="bg-[#222030] py-8 sm:min-w-[62rem] relative sm:pb-32 sm:rounded-md">
+      <div class="bg-[#222030] py-8 sm:min-w-[62rem] relative sm:pb-24 sm:rounded-md">
         <div
           class="flex flex-col justify-center items-center sm:absolute sm:right-1/2 sm:translate-x-1/2 sm:top-0 sm:-translate-y-1/2"
         >
@@ -78,19 +87,43 @@
             <div class="flex flex-col">
               <label class="text-white mb-1">{{ $t('sessions.username') }}</label>
               <div class="relative sm:flex sm:items-center gap-8">
-                <Field
-                  name="username"
-                  type="text"
-                  class="bg-transparent sm:bg-red-500 pb-2 border-b placeholder:text-white sm:placeholder:text-[#212529] text-white outline-none sm:py-1 sm:px-3 w-full sm:rounded-[4px]"
-                  disabled
-                />
+                <div class="sm:w-full">
+                  <Field
+                    name="username"
+                    type="text"
+                    class="bg-transparent sm:bg-red-500 pb-2 border-b placeholder:text-white sm:placeholder:text-[#212529] text-white outline-none sm:py-1 sm:px-3 w-full sm:rounded-[4px]"
+                    disabled
+                  />
+                </div>
                 <span
-                  @click="openEditForm"
-                  class="text-[#CED4DA] absolute sm:static right-0 top-[40%] -translate-y-1/2 sm:-translate-y-0 cursor-pointer"
+                  @click="() => openMode(Mode.EDIT)"
+                  class="text-[#CED4DA] absolute sm:static right-0 top-[40%] -translate-y-1/2 sm:-translate-y-0 cursor-pointer sm:hidden"
+                  >{{ $t('texts.edit') }}</span
+                >
+                <span
+                  @click="addNewUserInput"
+                  class="text-[#CED4DA] absolute sm:static right-0 top-[40%] -translate-y-1/2 sm:-translate-y-0 cursor-pointer hidden sm:block"
+                  >{{ $t('texts.edit') }}</span
+                >
+              </div>
+
+              <!-- Additional -->
+              <div class="relative sm:flex sm:items-center gap-8 mt-8" v-if="updateUsername">
+                <div class="sm:w-full">
+                  <label class="text-white mb-1">{{ $t('sessions.new_username') }}</label>
+                  <Field
+                    name="new_username"
+                    type="text"
+                    class="bg-transparent sm:bg-red-500 pb-2 border-b placeholder:text-white sm:placeholder:text-[#212529] text-white outline-none sm:py-1 sm:px-3 w-full sm:rounded-[4px]"
+                  />
+                </div>
+                <span
+                  class="text-[#CED4DA] absolute opacity-0 sm:static right-0 top-[40%] -translate-y-1/2 sm:-translate-y-0 cursor-pointer"
                   >{{ $t('texts.edit') }}</span
                 >
               </div>
             </div>
+
             <div class="flex flex-col">
               <label class="text-white mb-1">{{ $t('sessions.email') }}</label>
               <div class="relative sm:flex sm:items-center gap-8">
@@ -117,14 +150,77 @@
                   disabled
                 />
                 <span
-                  @click="openResetPasswordForm"
-                  class="text-[#CED4DA] absolute sm:static right-0 top-[40%] -translate-y-1/2 sm:-translate-y-0 sm:py-1"
+                  @click="() => openMode(Mode.RESET)"
+                  class="text-[#CED4DA] absolute sm:static right-0 top-[40%] -translate-y-1/2 sm:-translate-y-0 sm:py-1 sm:hidden"
                   >{{ $t('texts.edit') }}</span
                 >
+                <span
+                  @click="addNewPasswordInput"
+                  class="text-[#CED4DA] absolute sm:static right-0 top-[40%] -translate-y-1/2 sm:-translate-y-0 sm:py-1 hidden sm:block"
+                  >{{ $t('texts.edit') }}</span
+                >
+              </div>
+              <div v-if="updatePassword" class="relative sm:flex sm:items-center gap-8">
+                <div
+                  class="border border-[#ced4da] border-opacity-20 hidden sm:block mt-8 px-8 py-5 rounded-md w-full"
+                >
+                  <p class="text-white">{{ $t('texts.password_contain') }}:</p>
+                  <ul class="mt-4">
+                    <li class="text-[#9C9A9A]">* 8 {{ $t('texts.password_validation_length') }}</li>
+                    <li class="text-[#9C9A9A]">* 15 {{ $t('texts.password_validation_case') }}</li>
+                  </ul>
+                </div>
+                <span
+                  class="text-[#CED4DA] absolute opacity-0 sm:static right-0 top-[40%] -translate-y-1/2 sm:-translate-y-0 cursor-pointer hidden sm:block"
+                  >{{ $t('texts.edit') }}</span
+                >
+              </div>
+
+              <!-- New Password -->
+              <div class="mt-8 hidden sm:block" v-if="updatePassword">
+                <div class="flex flex-col">
+                  <label class="text-white mb-1">{{ $t('sessions.new_password') }}</label>
+                  <div class="relative sm:flex sm:items-center gap-8">
+                    <div class="sm:w-full">
+                      <Field
+                        name="new_password"
+                        type="password"
+                        class="bg-transparent sm:bg-red-500 pb-2 border-b placeholder:text-white sm:placeholder:text-[#212529] text-white outline-none sm:py-1 sm:px-3 w-full sm:rounded-[4px]"
+                      />
+                    </div>
+                    <span
+                      class="text-[#CED4DA] opacity-0 absolute sm:static right-0 top-[40%] -translate-y-1/2 sm:-translate-y-0 cursor-pointer hidden sm:block"
+                      >{{ $t('texts.edit') }}</span
+                    >
+                  </div>
+                </div>
+
+                <div class="flex flex-col mt-8">
+                  <label class="text-white mb-1">{{ $t('sessions.confirm_new_password') }}</label>
+                  <div class="relative sm:flex sm:items-center gap-8">
+                    <div class="sm:w-full">
+                      <Field
+                        name="confirm_new_password"
+                        type="password"
+                        class="bg-transparent sm:bg-red-500 pb-2 border-b placeholder:text-white sm:placeholder:text-[#212529] text-white outline-none sm:py-1 sm:px-3 w-full sm:rounded-[4px]"
+                      />
+                    </div>
+                    <span
+                      class="text-[#CED4DA] opacity-0 absolute sm:static right-0 top-[40%] -translate-y-1/2 sm:-translate-y-0 cursor-pointer hidden sm:block"
+                      >{{ $t('texts.edit') }}</span
+                    >
+                  </div>
+                </div>
               </div>
             </div>
           </div>
         </div>
+      </div>
+      <div class="justify-end mt-6 gap-8 hidden sm:flex" v-if="updateUsername">
+        <button class="text-[#CED4DA]" @click="cancelChanges">Cancel</button>
+        <button class="bg-[#E31221] text-white px-5 py-1.5 rounded-md" @click="confirmReset">
+          Save changes
+        </button>
       </div>
     </div>
   </TheLayout>
@@ -135,57 +231,99 @@ import GoBackBtn from '@/components/icons/GoBackBtn.vue'
 import profile from '@/assets/images/profile.png'
 import TheLayout from '@/components/TheLayout.vue'
 import { ref, onMounted } from 'vue'
-import { useForm, Field, Form } from 'vee-validate'
+import { useForm, Field } from 'vee-validate'
 import { useUserSessionStore } from '@/stores/UserSessionStore'
 import CustomInput from '@/components/Form/CustomInput.vue'
+import { useRouter } from 'vue-router'
 
+// States
 const userSession = useUserSessionStore()
-
+const router = useRouter()
 const username = ref('')
 const email = ref('')
-const edit = ref(false)
-const name = ref('')
-const finalCheck = ref(false)
-const foo = ref(true)
-const reset = ref(false)
-const { setValues } = useForm()
+const updateUsername = ref(false)
+const updatePassword = ref(false)
 
-const handleEdit = () => {
-  edit.value = false
-  finalCheck.value = true
-  reset.value = false
+const Mode = {
+  MAIN: 'main',
+  EDIT: 'edit',
+  RESET: 'reset',
+  FINAL_CHECK: 'finalCheck'
 }
 
-const openResetPasswordForm = () => {
-  reset.value = true
-  foo.value = false
+const initialValues = ref({ username: '', email: '', password: '**********' })
+const currentMode = ref(Mode.MAIN)
+const { setValues, handleSubmit } = useForm()
+
+// Functions
+const openMode = (mode: string) => {
+  currentMode.value = mode
 }
 
-const openEditForm = () => {
-  foo.value = false
-  edit.value = true
+const closeEditForm = () => {
+  currentMode.value = Mode.MAIN
+
+  setValues({
+    username: username.value,
+    email: email.value,
+    password: initialValues.value.password
+  })
 }
 
-const closeEditFrom = () => {
-  foo.value = true
-  edit.value = false
-  finalCheck.value = false
+const confirmEdit = () => {
+  openMode(Mode.FINAL_CHECK)
 }
 
-const onSubmit = () => {
-  closeEditFrom()
+const confirmReset = () => {
+  openMode(Mode.FINAL_CHECK)
 }
 
+const onSubmit = handleSubmit((values) => {
+  console.log('Form Values:', values)
+  console.log('Username:', values.username)
+  console.log('Password:', values.password)
+  console.log('Password Confirmation:', values.password_confirmation)
+
+  closeEditForm()
+})
+
+const foo = () => {
+  onSubmit()
+}
+
+const addNewUserInput = () => {
+  updateUsername.value = true
+}
+
+const addNewPasswordInput = () => {
+  updatePassword.value = true
+}
+
+const cancelChanges = () => {
+  updatePassword.value = false
+  updateUsername.value = false
+}
+
+const goBack = () => {
+  router.go(-1)
+}
+
+// Life-cycles
 onMounted(async () => {
   if (localStorage.getItem('isLoggedIn')) {
     await userSession.getUserData()
     username.value = userSession.userData.username
     email.value = userSession.userData.email
   }
-  setValues({
+  initialValues.value = {
     username: username.value,
     email: email.value,
     password: '**********'
+  }
+  setValues({
+    username: username.value,
+    email: email.value,
+    password: initialValues.value.password
   })
 })
 </script>
